a57e106fa2910e4e6c2010304e5c1ad4b1595a19
IgorGarciaCosta <igonildo7@gmail.com>
2020-09-08 15:58:09 -0300

    some corrections

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,1 @@
 console.log('Client side javascript file is loaded!')
-
-var slideIndex = 1;
-showDivs(slideIndex); //display first page
-
-function plusDivs(n) { //called when one of the buttons is clicked
-    showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-    var i;
-    var allSLides = document.getElementsByClassName("mySlides");
-    if (n > allSLides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = allSLides.length };
-    for (i = 0; i < allSLides.length; i++) {
-        allSLides[i].style.display = "none"; //hidden image
-    }
-    allSLides[slideIndex - 1].style.display = "block"; //shown image
-}
